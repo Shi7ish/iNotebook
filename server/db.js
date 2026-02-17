4cc44566ae56d7c11d77f17ac3eec696c5a51406
Shi7ish <shirish07thore@gmail.com>
2026-02-17 23:40:28 +0530

    changes

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/inotebook';
+const mongoURI = 'mongodb+srv://shirishthore07:Shirish1123@cluster0.zhjoag1.mongodb.net/?appName=Cluster0';
 const connectToMongo = async () => {
     await mongoose.connect(mongoURI);
     console.log('Connected to MongoDB');

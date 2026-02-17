b652a452deac46046b4a74cc126e48e556d713f1
Shi7ish <shirish07thore@gmail.com>
2026-02-17 23:10:48 +0530

    third commit

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -12,7 +12,7 @@ const SignUp = (props) => {
   const handleOnSubmit = async (e) => {
     e.preventDefault();
     const { name, email, password } = credentials;
-    const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+    const response = await fetch(`https://inotebook-backend-psi-five.vercel.app/api/auth/createuser`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

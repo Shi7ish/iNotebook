1cfc3a60cffd19ce0dbe3d5334a1583d601d7bde
Shi7ish <shirish07thore@gmail.com>
2026-02-17 23:17:58 +0530

    fourth commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,3 @@ app.use(express.json())
 // Available routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
